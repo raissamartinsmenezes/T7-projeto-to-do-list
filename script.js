e736dfc39e620b5d7154bc41c19b3643f336028a
raissamartinsmenezes <raissamartinsdemenezes@gmail.com>
2019-05-02 14:58:58 -0300

    finalização projeto to-do-list

--- script.js
+++ script.js
@@ -3,35 +3,60 @@ let adicionarTarefa = document.getElementById('adicionarTarefa')
 let adicionarBotoes = document.getElementById('adicionarBotoes')
 let concluirTudo = document.getElementById('concluirTudo')
 let apagarTudo = document.getElementById('apagarTudo')
+let dragging
 
-
-adicionarTarefa.addEventListener('click', function(){
+adicionarTarefa.addEventListener('click', function () {
 
     if (inserirTarefa.value == false) {
-        return 
+        return
     }
 
     let listaDeTarefas = document.getElementById('listaDeTarefas')
+
+    const divDrag = document.createElement("div")
+    divDrag.className = "div-drag"
+    divDrag.setAttribute('draggable', true)
 
     let tarefa = document.createElement('p')
     tarefa.innerHTML = inserirTarefa.value
     listaDeTarefas.appendChild(tarefa)
 
     let excluir = document.createElement('button')
-    excluir.innerHTML = 'x'
+    excluir.innerHTML = '🗑️'
     excluir.classList = 'excluir-botao'
     tarefa.appendChild(excluir)
 
     adicionarBotoes.classList.remove('botoes')
 
     excluir.addEventListener('click', function () {
-        tarefa.remove()
+        // tarefa.remove()
+        divDrag.remove()
     })
+
+    tarefa.innerHTML = inserirTarefa.value
+    divDrag.appendChild(tarefa)
+    tarefa.appendChild(excluir)
+    listaDeTarefas.appendChild(divDrag)
 
     inserirTarefa.value = ''
 
-    tarefa.addEventListener('click', function(){
-        tarefa.classList.add('check');
+    tarefa.addEventListener('click', function () {
+        tarefa.classList.toggle('check');
+    })
+
+    divDrag.addEventListener("dragstart", function (ev) {
+        dragging = ev.target.closest(".div-drag")
+
+    })
+
+    divDrag.addEventListener("dragover", function (ev) {
+        ev.preventDefault();
+        const node = ev.target.closest(".div-drag")
+        this.parentNode.insertBefore(dragging, node)
+    })
+
+    divDrag.addEventListener("dragend", function (ev) {
+        dragging = null
     })
 
     concluirTudo.addEventListener('click', function () {
@@ -39,9 +64,10 @@ adicionarTarefa.addEventListener('click', function(){
     })
 
     apagarTudo.addEventListener('click', function () {
-        tarefa.remove()
+        // tarefa.remove()
+        divDrag.remove()
         adicionarBotoes.classList.add('botoes')
     })
- 
+
 })
 
